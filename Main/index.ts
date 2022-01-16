953abcb6985995f3a205442005e1e13c338413c2
unknown <guyrdmello@gmail.com>
2022-01-16 15:39:49 -0500

    adding embeds

--- Main/index.ts
+++ Main/index.ts
@@ -1,9 +1,12 @@
 import DiscordJS, { Intents } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
+
 const axios = require('axios')
 const fetch = require('node-fetch')
 const prefix = '!'
+const Default_Cities = ['toronto', 'missisuagua', 'brampton', 'vaughan']
+const { MessageEmbed } = require('discord.js');
 
 const client = new DiscordJS.Client({
     intents: [
@@ -22,7 +25,7 @@ client.on("messageCreate", async msg => {
     }
     const args = msg.content.slice(prefix.length).trim().split(/ +/g)
     const command = args.shift()?.toLowerCase()
-    console.log(args)
+    // console.log(args)
 
     if (command === "ego") {
         msg.react("😃")
@@ -49,27 +52,141 @@ client.on("messageCreate", async msg => {
         const combinedArgs = args.join(" ")
         msg.channel.send(`${msg.author.username} is angry and has something to say: \n\n ${combinedArgs.toUpperCase()}`)
     }
-    if (command === 'weather') {
+    if (command === 'weather' && args[0]) {
         let getWeather = async () => {
-            let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&appid=${process.env.API_KEY}`)
+            let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&units=metric&appid=${process.env.API_KEY}`)
             let json = await result.json()
             return json
         }
-        let weather = await getWeather()
+        let weather = await getWeather() 
+        console.log(weather.coord.lon)
+        const weatherEmbed = {
+            color: 0x0099ff,
+            title: 'Weather',
+            url: 'https://discord.com/api/oauth2/authorize?client_id=932030243768795178&permissions=534723947584&scope=bot',
+            author: {
+                name: 'RainMan',
+                icon_url: `http://openweathermap.org/img/w/${weather.weather.icon}.png`,
+                url: 'https://discord.com/api/oauth2/authorize?client_id=932030243768795178&permissions=534723947584&scope=bot',
+            },
+            description: 'Some description here',
+            thumbnail: {
+                url: 'https://i.imgur.com/AfFp7pu.png',
+            },
+            fields: [
+                {
+                    name: 'Regular field title',
+                    value: 'Some value here',
+                },
+                {
+                    name: '\u200b',
+                    value: '\u200b',
+                    inline: false,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+            ],
+            image: {
+                url: 'https://i.imgur.com/AfFp7pu.png',
+            },
+            timestamp: new Date(),
+            footer: {
+                text: 'Some footer text here',
+                icon_url: 'https://i.imgur.com/AfFp7pu.png',
+            },
+        };
+        // msg.channel.send(`Here's Your Weather ${Math.floor(weather.main.temp)}, but it feels like ${Math.floor(weather.main.feels_like)} and 
+        // ${weather.weather.icon}`)
+        msg.channel.send({ embeds: [weatherEmbed] })
 
-        console.log(weather)    
-        msg.channel.send(`Here's Your Weather ${Math.round(weather.main.temp - 273.15)}, but it feels like ${Math.round(weather.main.feels_like - 273.15)}`)
+        //http://openweathermap.org/img/w/10d.png
+        // msg.channel.send(`Here's Your Weather ${Math.round(weather.main.temp - 273.15)}, but it feels like ${Math.round(weather.main.feels_like - 273.15)}`)
+    } else if (command === 'weather' && !args[0]) {
+        // let getWeather = async () => {
+        //     let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&units=metric&appid=${process.env.API_KEY}`)
+        //     let json = await result.json()
+        //     return json
+        // }
+        // let weather = await getWeather() 
+        // msg.channel.send(`Here's Your Weather ${Math.floor(weather.main.temp)}, but it feels like ${Math.floor(weather.main.feels_like)} and 
+        // ${weather.weather.icon}`)
+
+
+        const City_Data = []
+        for (let i = 0; i < Default_Cities.length; i++) {
+            console.log (Default_Cities[i]);
+            let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&units=metric&appid=${process.env.API_KEY}`)
+            let json = await result.json()
+            City_Data.push(json)
+          }
+        console.log(City_Data)
+        console.log(Default_Cities)
     }
-    // if (command === 'weather') {
-    //     let getWeather = async () => {
-    //         let response = await axios.defualt.get('api.openweathermap.org/data/2.5/weather?q=toronto&appid=3151f2ed2e02e5ae323613f34a00f652')
-    //         let weather = response.data
-    //         return weather
-    //     }
-    //     let WeatherValue = await getWeather()
-    //     console.log(WeatherValue)
-    //     msg.channel.send("hi")
-    // }
+
+    if(command === "test") {
+        const exampleEmbed = {
+            color: 0x0099ff,
+            title: 'Weather',
+            url: 'https://discord.com/api/oauth2/authorize?client_id=932030243768795178&permissions=534723947584&scope=bot',
+            author: {
+                name: 'RainMan',
+                icon_url: 'https://i.imgur.com/AfFp7pu.png',
+                url: 'https://discord.com/api/oauth2/authorize?client_id=932030243768795178&permissions=534723947584&scope=bot',
+            },
+            description: 'Some description here',
+            thumbnail: {
+                url: 'https://i.imgur.com/AfFp7pu.png',
+            },
+            fields: [
+                {
+                    name: 'Regular field title',
+                    value: 'Some value here',
+                },
+                {
+                    name: '\u200b',
+                    value: '\u200b',
+                    inline: false,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+            ],
+            image: {
+                url: 'https://i.imgur.com/AfFp7pu.png',
+            },
+            timestamp: new Date(),
+            footer: {
+                text: 'Some footer text here',
+                icon_url: 'https://i.imgur.com/AfFp7pu.png',
+            },
+        };
+        msg.channel.send({ embeds: [exampleEmbed] });
+    }
 })
 
 client.login(process.env.TOKEN)
