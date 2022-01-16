eb32e88a5d1293a6b4fea53e6408227f5bdc8129
unknown <guyrdmello@gmail.com>
2022-01-16 14:05:39 -0500

    commands

--- Main/index.ts
+++ Main/index.ts
@@ -1,6 +1,8 @@
 import DiscordJS, { Intents } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
+const axios = require('axios')
+const prefix = '!'
 
 const client = new DiscordJS.Client({
     intents: [
@@ -13,11 +15,38 @@ client.on('ready', () => {
     console.log('The bot is ready')
 })
 
-client.on('messageCreate', (message) => {
-    if (message.content === 'ping') {
-        message.reply({
-            content: 'pong',
+client.on("messageCreate", msg => {
+    if(!msg.content.startsWith(prefix)) {
+        return
+    }
+    const args = msg.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift()?.toLowerCase()
+    console.log(args)
+
+    if (command === "ego") {
+        msg.react("😃")
+    }
+
+    if (command === 'ping') {
+        msg.reply({
+            content: 'Don',
         })
+    }
+
+    if(command === 'clear') {
+        let num = 256
+        if (args[0]) {
+            num = parseInt(args[0]) + 1
+        }
+        if (msg.channel.type === "GUILD_TEXT") {
+            msg.channel.bulkDelete(num)
+            msg.channel.send(`bot deleted ${args[0]} messages for you`)
+        }
+    }
+
+    if (command === 'capslock') {
+        const combinedArgs = args.join(" ")
+        msg.channel.send(`${msg.author.username} is angry and has something to say: \n\n ${combinedArgs.toUpperCase()}`)
     }
 })
 
