32c0ac92b3037d975d5eb6d264bb452b3a64fc8f
unknown <guyrdmello@gmail.com>
2022-01-15 23:09:52 -0500

    Basic text message bot

---
+++ Main/index.ts
@@ -0,0 +1,24 @@
+import DiscordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.on('ready', () => {
+    console.log('The bot is ready')
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'Kavan likes men',
+        })
+    }
+})
+
+client.login(process.env.TOKEN)
